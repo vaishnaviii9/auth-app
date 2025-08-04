0e36066f33f36099dca478ea434255e049eb3107
Vaishnavi <vaishnavitonpe0@gmail.com>
2025-08-04 09:09:46 -0400

    feat: Add user login functionality with JWT authentication and toast notifications

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,59 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   const onLogin = async () => {
     // Implement your login logic here
+    try {
+      setLoading(true)
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+
+      toast.success("Login success")
+
+      router.push("/profile")
+      
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(true);
+    }
   };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-blue-400 to-purple-500">
       <div className="p-10 rounded-lg shadow-2xl bg-white w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Login</h1>
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Login
+        </h1>
         <hr className="mb-6 border-gray-300" />
 
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
@@ -35,7 +67,10 @@ export default function LoginPage() {
         </div>
 
         <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
