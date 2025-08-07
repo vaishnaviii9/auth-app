429ab01e0631a113b0d118ad7adee2106e4a72a4
Vaishnavi <vaishnavitonpe0@gmail.com>
2025-08-07 10:29:17 -0400

    feat: Implement user profile retrieval and middleware for authentication

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
     try {
@@ -18,12 +20,24 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
-
+      <h2 className="p-3 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}> {data}</Link>
+        )}
+      </h2>
       <hr />
 
       <button
@@ -38,6 +52,20 @@ export default function ProfilePage() {
       rounded"
       >
         Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 
+        mt-4
+        hover:bg-blue-700
+      text-white
+      font-bold
+      py-2
+      px-4
+      rounded"
+      >
+        Get User Details
       </button>
     </div>
   );
